e89398ae2d3ad049ab021b402c0823d0b15ac4b4
Ehasanul Haque <ehasan.best@gmail.com>
2023-05-21 20:18:48 +0600

    updateed js and css

--- script.js
+++ script.js
@@ -6,19 +6,23 @@ const toastDetails = {
     timer: 5000,
     success: {
         icon: "fa-check-circle",
-        text: "Success: This is a success toast.",
+        capName: "Success: ",
+        text: "This is a success toast.",
     },
     error: {
         icon: "fa-times-circle",
-        text: "Error: This is an error toast.",
+        capName: "Error: ",
+        text: "This is an error toast.",
     },
     warning: {
         icon: "fa-exclamation-triangle",
-        text: "Warning: This is a warning toast.",
+        capName: "Warning: ",
+        text: "This is a warning toast.",
     },
     info: {
         icon: "fa-info-circle",
-        text: "Info: This is an info toast.",
+        capName: "Info: ",
+        text: "This is an info toast.",
     }
 }
 const removeToast = (toast) => {
@@ -28,12 +32,12 @@ const removeToast = (toast) => {
 }
 
 const createToast = (id) => {
-    const { icon, text } = toastDetails[id];
+    const { icon, capName, text } = toastDetails[id];
     const toast = document.createElement("li");
     toast.className = `toast ${id}`;
     toast.innerHTML = `<div class="coulmn">
                             <i class="fa ${icon}"></i></i>
-                            <span>${text}</span>
+                            <span><b>${capName}</b> ${text}</span>
                         </div>
                         <i class="fa fa-times" onclick="removeToast(this.parentElement)"></i>`;
     notifications.appendChild(toast);
